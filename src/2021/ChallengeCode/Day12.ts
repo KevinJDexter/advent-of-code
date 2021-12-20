f0e70475f952d878f01e28e93eefaaab5f2bef07
Dexter <kevin.dexter@merrillcorp.com>
2021-12-20 12:00:12 -0600

    Day 12

--- src/2021/ChallengeCode/Day12.ts
+++ src/2021/ChallengeCode/Day12.ts
@@ -1,3 +1,42 @@
+const numPathsToEnd = (currentRoom: string, remainingPaths: string[], roomToRevisit: string, canRevisitASmallRoom: boolean, roomRevisited: boolean, currentPath: string) => {
+  if (currentRoom === 'end') {
+    if (roomToRevisit !== ''  && !roomRevisited) {
+      return 0;
+    }
+    return 1;
+  }
+  if (currentPath[currentPath.length - 3] === 'd') {console.log(currentPath)}
+  let numberPaths = 0;
+  const remainingPathsWithCurrentRoom = remainingPaths.filter((path: string) => path.split('-').includes(currentRoom));
+  let remainingPathsAfterCurrentRoom = [...remainingPaths];
+  let willRevisitThisRoom = false;
+  let markRoomRevisited = roomRevisited;
+  if (currentRoom.toLocaleLowerCase() === currentRoom) {
+    if (canRevisitASmallRoom && roomToRevisit === '' && currentRoom !== 'start') {
+      willRevisitThisRoom = true;
+    } else if (canRevisitASmallRoom && roomToRevisit === currentRoom) {
+      markRoomRevisited = true;
+    } else if (canRevisitASmallRoom) {
+    }
+    remainingPathsAfterCurrentRoom = remainingPaths.filter((path: string) => !path.split('-').includes(currentRoom));
+  }
+  const nextRooms = remainingPathsWithCurrentRoom.map((path: string) => {
+    let rooms = path.split('-');
+    return rooms[1-rooms.indexOf(currentRoom)];
+  })
+  nextRooms.forEach((room: string) => {
+    if (currentRoom === 'd') {
+    }
+    numberPaths += numPathsToEnd(room, remainingPathsAfterCurrentRoom, roomToRevisit, canRevisitASmallRoom, markRoomRevisited, currentPath + '-' + room);
+    if (willRevisitThisRoom && room !== 'end') {
+      numberPaths += numPathsToEnd(room, remainingPaths, currentRoom, canRevisitASmallRoom, markRoomRevisited, currentPath + '-' + room)
+    }
+  });
+  return numberPaths;
+}
+
 export const Day12 = (input: string[]) => {
   console.log("Day12");
+  console.log("Paths when restricted to one visit to small rooms:", numPathsToEnd('start', input, '', false, false, 'start'));
+  console.log("Paths when restricted to two visits to small rooms:", numPathsToEnd('start', input, '', true, false, 'start'));
 }
--- src/Main.ts
+++ src/Main.ts
@@ -13,4 +13,4 @@ const main = (day: string, year: string) => {
   YearMethods[`Year${year}`][day](input)
 }
 
-main("Day11", "2021");
+main("Day12", "2021");
